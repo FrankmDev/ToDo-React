4d706258ba87f7d66df6648226e9935e676e7c55
FrankmDev <frankmmarindev@gmail.com>
2023-11-08 13:00:58 +0100

    Header

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h2 className="primary">All Tasks</h2>
+      <span className="stats">0/10 Tasks Done</span>
+    </header>
+  );
+};
+
+export default Header;
