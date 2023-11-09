b6566d2f0d33ed12aadac17a9e356b0b8e8910a8
FrankmDev <frankmmarindev@gmail.com>
2023-11-09 19:25:52 +0100

    Tasks

--- src/Tasks/NewTask.jsx
+++ src/Tasks/NewTask.jsx
@@ -6,11 +6,15 @@ const NewTask = () => {
   return (
     <section className="newTask-section">
       <div className="task">
-        <FaReact />
-        <h3 className="task-title">Futbol</h3>
-        <p className="task-description">Play Futbol with my fck parents</p>
+        <div className="task-text">
+          <h3 className="task-title">Futbol</h3>
+          <p className="task-description">Play Futbol with my fck parents</p>
+        </div>
+        <FaReact className="react-icon" />
       </div>
-      <IoClose />
+      <button className="closeBtn">
+        <IoClose className="closeIcon" />
+      </button>
     </section>
   );
 };
